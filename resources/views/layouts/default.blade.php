8e6ec8cbe06f6cf3f5af119e200653d7e9e7a060
Lxi <luoluo15577299230>
2018-04-26 16:56:29 +0000

    add test-page

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>@yield('title', 'Sample_2018') - Laravel Study_Test</title>
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
