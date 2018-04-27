cc74e90425cb721890866ab1d10cf8b434068a83
Lxi <luoluo15577299230>
2018-04-27 16:59:16 +0000

    Finish Home page

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,5 +2,17 @@
 
 @section('title', 'home')
 @section('content')
-    <h1>小萝卜你咋这么厉害捏</h1>
+    <div class="jumpbtron">
+        <h1>Hello 小萝卜</h1>
+        <p class="lead">
+            你现在所看到的是 <a href="https://laravel-china.org/courses/laravel-essential-training-5.1">Laravel PHP Sample_2018示例项目主页。</a>
+        </p>
+        <p>
+            Let's Begining!
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="#" role="button">Login now</a>
+        </p>
+    </div>
+
 @stop
